fd2e97f5aa79083340e7b9f45100818200a28ac0
Justin Tangas <jtangas@gmail.com>
2018-05-25 21:00:34 -0700

    adding some stuff

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jtangas
+ * Date: 5/25/18
+ * Time: 8:57 PM
+ */
+
+namespace Jtangas\CoreBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class BaseController extends Controller
+{
+    public function handleRequest()
+    {
+
+    }
+
+    public function parseRequest(Request $request)
+}
